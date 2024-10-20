585545cdfeb982108b3276b4924a56ea36831788
solebellsBEACH <lucassxxavier@gmail.com>
2024-10-19 23:21:38 -0300

    feat: adding new fields

--- src/pages/register/components/RegisterForm/index.tsx
+++ src/pages/register/components/RegisterForm/index.tsx
@@ -1,27 +1,29 @@
-import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { FaUser, FaLock } from "react-icons/fa";
 import { Button, Container, FormWrapper, IconWrapper, Input, InputField, RegisterLink, SignupText, Title } from "./styles";
 import { useRouter } from "next/router";
 
+type FormValues = {
+    name: string;
+    email: string;
+    address: string;
+    description: string;
+    phone: string;
+    rate: number;
+    hour_range: {
+        init_hour: string;
+        finish_hour: string;
+    };
+    available_services: string[];
+    experience: string;
+};
+
 export function RegisterForm() {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<FormValues>();
     const router = useRouter();
 
-    const [fullName, setFullName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [role, setRole] = useState("Cliente");
-
-    const handleSubmit = (event: any) => {
-        event.preventDefault();
-
-        if (password !== confirmPassword) {
-            alert("As senhas não coincidem.");
-            return;
-        }
-
-        console.log("Dados de Registro:", { fullName, email, password, role });
-
+    const onSubmit: SubmitHandler<FormValues> = (data) => {
+        console.log("Dados de Registro:", data);
         router.push("/");
     };
 
@@ -29,7 +31,7 @@ export function RegisterForm() {
         <Container>
             <FormWrapper>
                 <Title>Registrar</Title>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <InputField>
                         <IconWrapper>
                             <FaUser />
@@ -37,10 +39,9 @@ export function RegisterForm() {
                         <Input
                             type="text"
                             placeholder="Nome Completo"
-                            required
-                            value={fullName}
-                            onChange={(e) => setFullName(e.target.value)}
+                            {...register("name", { required: true })}
                         />
+                        {errors.name && <span>Nome é obrigatório</span>}
                     </InputField>
 
                     <InputField>
@@ -50,36 +51,81 @@ export function RegisterForm() {
                         <Input
                             type="email"
                             placeholder="E-mail"
-                            required
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
+                            {...register("email", { required: true })}
                         />
+                        {errors.email && <span>E-mail é obrigatório</span>}
                     </InputField>
 
                     <InputField>
                         <IconWrapper>
-                            <FaLock />
+                            <FaUser />
                         </IconWrapper>
                         <Input
-                            type="password"
-                            placeholder="Senha"
-                            required
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
+                            type="text"
+                            placeholder="Endereço"
+                            {...register("address", { required: true })}
                         />
+                        {errors.address && <span>Endereço é obrigatório</span>}
                     </InputField>
 
                     <InputField>
                         <IconWrapper>
-                            <FaLock />
+                            <FaUser />
                         </IconWrapper>
                         <Input
-                            type="password"
-                            placeholder="Confirmar Senha"
-                            required
-                            value={confirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            type="text"
+                            placeholder="Descrição"
+                            {...register("description", { required: true })}
                         />
+                        {errors.description && <span>Descrição é obrigatória</span>}
+                    </InputField>
+
+                    <InputField>
+                        <IconWrapper>
+                            <FaUser />
+                        </IconWrapper>
+                        <Input
+                            type="text"
+                            placeholder="Telefone"
+                            {...register("phone", { required: true })}
+                        />
+                        {errors.phone && <span>Telefone é obrigatório</span>}
+                    </InputField>
+
+                    <InputField>
+                        <IconWrapper>
+                            <FaUser />
+                        </IconWrapper>
+                        <Input
+                            type="number"
+                            placeholder="Taxa (R$)"
+                            {...register("rate", { required: true })}
+                        />
+                        {errors.rate && <span>Taxa é obrigatória</span>}
+                    </InputField>
+
+                    <InputField>
+                        <IconWrapper>
+                            <FaUser />
+                        </IconWrapper>
+                        <Input
+                            type="text"
+                            placeholder="Hora de Início"
+                            {...register("hour_range.init_hour", { required: true })}
+                        />
+                        {errors.hour_range?.init_hour && <span>Hora de início é obrigatória</span>}
+                    </InputField>
+
+                    <InputField>
+                        <IconWrapper>
+                            <FaUser />
+                        </IconWrapper>
+                        <Input
+                            type="text"
+                            placeholder="Hora de Término"
+                            {...register("hour_range.finish_hour", { required: true })}
+                        />
+                        {errors.hour_range?.finish_hour && <span>Hora de término é obrigatória</span>}
                     </InputField>
 
                     <InputField>
@@ -87,9 +133,8 @@ export function RegisterForm() {
                             <FaUser />
                         </IconWrapper>
                         <select
-                            value={role}
-                            onChange={(e) => setRole(e.target.value)}
-                            required
+                            {...register("available_services", { required: true })}
+                            multiple
                             style={{
                                 width: "100%",
                                 padding: "0.625rem 2.5rem 0.625rem 2.5rem",
@@ -100,9 +145,23 @@ export function RegisterForm() {
                                 outline: "none",
                             }}
                         >
-                            <option value="Cliente">Cliente</option>
-                            <option value="Prestador">Prestador</option>
+                            <option value="Serviço 1">Serviço 1</option>
+                            <option value="Serviço 2">Serviço 2</option>
+                            <option value="Serviço 3">Serviço 3</option>
                         </select>
+                        {errors.available_services && <span>Selecione ao menos um serviço</span>}
+                    </InputField>
+
+                    <InputField>
+                        <IconWrapper>
+                            <FaUser />
+                        </IconWrapper>
+                        <Input
+                            type="text"
+                            placeholder="Experiência"
+                            {...register("experience", { required: true })}
+                        />
+                        {errors.experience && <span>Experiência é obrigatória</span>}
                     </InputField>
 
                     <Button type="submit">Registrar</Button>
